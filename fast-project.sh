02b4c7d6ce3f40bbe76f40f64a082b9bc68ae668
falconcodes <prathamesh.chavanpsc2018@gmail.com>
2024-04-03 21:32:19 +0530

    feat: generate full stack express compose file

--- fast-project.sh
+++ fast-project.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 
 fastproject() {
-    local project_name=$1
-    sed "s/project_client/${project_name}_client/" $PWD/docker-compose.yml  | sed "s/project_server/${project_name}_server/"
+    local project_template=$1
+    local project_name=$2
+    local template_compose_file=$HOME/keep-coding/fast-project-package/expressjs-client-server-template/docker-compose.yml
+
+    # for d in */ ; do
+    #     echo "$d"
+    #     if [ project_name = "$d" ]; then
+    #         echo "matched"
+    #     fi
+    # done
+
+    # sed "s/project_client/${project_name}_client/" $template_compose_file | sed "s/project_server/${project_name}_server/" | sed "s/project_db_volume/${project_name}_db_volume/" > $PWD/docker-compose.yml | bat $PWD/docker-compose.yml
+
+    sed "s/project_/${project_name}_/" $template_compose_file > $PWD/docker-compose.yml | bat $PWD/docker-compose.yml
+
 }
+
+# Scaffolding project
 
